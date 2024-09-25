4bd5822f3622462dc28004118750d97cf8d8112a
robnarley <robb09201@gmail.com>
2024-09-26 08:32:08 +1000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "0x66Cce77d08FbBb902C7397c5D2CD9CCbb3Ad1C4C";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x2c301A5Fa1e33f3b02a63d9E805Be83d2E20fC5D";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
